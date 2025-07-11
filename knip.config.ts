5260e4f8a1b13065c5b4912229ad886359051c35
Yuta Kobayashi <hi@yutakobayashi.com>
2025-07-12 06:06:21 +0900

    chore: install lefthook and secretlint (#3)
    * chore: install secretlint
    
    * fix: oxlint and eslint
    
    * fix
    
    * fix: fmt

--- knip.config.ts
+++ knip.config.ts
@@ -1,14 +1,13 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  ignoreUnresolved: [
-    '\\+types/.+',
-  ],
+  ignoreUnresolved: ["\\+types/.+"],
   ignore: ["eslint.config.js", "worker-configuration.d.ts", "workers/app.ts"],
   ignoreDependencies: [
     "tailwindcss",
     "eslint-plugin-oxlint",
     "@react-router/node",
+    "@secretlint/secretlint-rule-preset-recommend",
   ],
   ignoreBinaries: ["oxlint", "eslint"],
 };
--- workers/app.ts
+++ workers/app.ts
@@ -3,27 +3,27 @@ import { createRequestHandler } from "react-router";
 import * as schema from "../database/schema";
 
 declare module "react-router" {
-    export interface AppLoadContext {
-        cloudflare: {
-            env: Env;
-            ctx: ExecutionContext;
-        };
-        db: DrizzleD1Database<typeof schema>;
-    }
+  export interface AppLoadContext {
+    cloudflare: {
+      env: Env;
+      ctx: ExecutionContext;
+    };
+    db: DrizzleD1Database<typeof schema>;
+  }
 }
 
 const requestHandler = createRequestHandler(
-    () => import("virtual:react-router/server-build"),
-    import.meta.env.MODE
+  () => import("virtual:react-router/server-build"),
+  import.meta.env.MODE,
 );
 
 export default {
-    async fetch(request, env, ctx) {
-        const db = drizzle(env.DB, { schema });
+  async fetch(request, env, ctx) {
+    const db = drizzle(env.DB, { schema });
 
-        return requestHandler(request, {
-            cloudflare: { env, ctx },
-            db,
-        });
-    },
+    return requestHandler(request, {
+      cloudflare: { env, ctx },
+      db,
+    });
+  },
 } satisfies ExportedHandler<Env>;
