e8929f7e6fc8d30c1ef2a2df96728cbb36c0eeb8
Yuta Kobayashi <hi@yutakobayashi.com>
2025-07-12 04:43:40 +0900

    chore: linter and fmt setup (#1)
    * chore: save-exact
    
    * chore: setup oxlint,prettier
    
    * test: ci
    
    * fix: pnpm
    
    * fix: missing commands
    
    * fix: knip
    
    * fix: ignoreUnresolved

---
+++ knip.config.ts
@@ -0,0 +1,17 @@
+import type { KnipConfig } from "knip";
+
+const config: KnipConfig = {
+  ignoreUnresolved: [
+    '\\+types/.+',
+  ],
+  ignore: ["eslint.config.js"],
+  ignoreDependencies: [
+    "tailwindcss",
+    "eslint-plugin-oxlint",
+    "isbot",
+    "@react-router/node",
+  ],
+  ignoreBinaries: ["oxlint", "eslint"],
+};
+
+export default config;
