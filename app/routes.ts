b46c393e1b05b33f32dd6f5786ac531d4e0cb775
yutakobaayshidev <hi@yutakobayashi.com>
2025-07-13 08:03:29 +0900

    feat: header

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,12 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  layout,
+} from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  layout("routes/layout.tsx", [index("routes/home.tsx")]),
   route("login/github", "routes/login.github.tsx"),
   route("login/github/callback", "routes/login.github.callback.tsx"),
 ] satisfies RouteConfig;
